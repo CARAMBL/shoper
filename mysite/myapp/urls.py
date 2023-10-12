511cdc333d3c3794f28af6d9f0b94630a62d6eef
CARAMBL <tsuba.109546@gmail.com>
2023-10-12 03:41:55 +0300

    les21

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index, Detail
+from .views import index, Detail, additem, updateitem, deleteitem
 
 app_name = "myapp"
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
     path('<int:id>/', Detail, name ='detail'),
+    path('additem/', additem, name ='additem'),
+    path('updateitem/<int:id>/', updateitem, name ='updateitem'),
+    path('deleteitem/<int:id>/', deleteitem, name ='deleteitem'),
 
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Product
 # Create your views here.
@@ -22,3 +22,41 @@ def Detail (request,id):
 
     return render(request, "myapp/detail.html", context)
 
+
+def additem(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        disc = request.POST.get("disc")
+        image = request.FILES['upload']
+        item = Product(name=name,price=price,disc=disc,image=image)
+        item.save()
+    return render(request, "myapp/additem.html")
+
+def updateitem(request,id):
+    item = Product.objects.get(id=id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.disc = request.POST.get("disc")
+        item.image = request.FILES.get('upload', item.image)
+        item.save()
+        return redirect("http://127.0.0.1:8000/")
+
+    context = {
+        'item': item
+    }
+
+    return render(request,"myapp/updateitem.html", context)
+
+
+def deleteitem(request, id):
+    item = Product.objects.get(id=id)
+    if request.method == "POST":
+        item.delete()
+        return redirect("http://127.0.0.1:8000/")
+
+    context = {
+        'item': item
+    }
+    return render(request, "myapp/deliteitem.html", context)
